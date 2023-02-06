c7bbb3197a17347ad1204b33a3e2b0b8f60fe0f8
swaroop37 <124040573+swaroop37@users.noreply.github.com>
2023-02-06 14:49:20 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,14 @@
+#largest among 3 numbers
+
+a=int(input("enter a number:"))
+b=int(input("enter a number:"))
+c=int(input("enter a number:"))
+if(a>b):
+    if(a>c):
+        print("a is greater ")
+    else:
+        print("c is max")
+elif (b>c):
+    print("b is max")
+        
+   
